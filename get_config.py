d66f81dda8d4334b7376bee16af34abd04b4fa2a
Matt Martin <matt92martin@gmail.com>
2018-01-03 16:29:08 -0800

    Changed to db set up

---
+++ get_config.py
@@ -0,0 +1,99 @@
+import os
+import sqlite3
+import sys
+
+
+def file_exists(fp):
+    return os.path.isfile(fp)
+
+
+def connect(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Exception, e:
+        print e
+    return None
+
+
+def create_table(conn, sql):
+    try:
+        c = conn.cursor()
+        c.execute(sql)
+    except Exception, e:
+        print e
+
+
+def create_user(conn, user):
+    sql = """INSERT INTO noter(user,location) VALUES(?,?)"""
+    location = ""
+    message = "Please enter the full path of your Noter file. (e.g., /home/user/noter.txt): "
+    while location == "":
+        enteredpath = raw_input(message)
+        if file_exists(enteredpath):
+            location = enteredpath
+        else:
+            message = "Invalid path! Please ensure your file is present. (e.g., /home/user/noter.txt): "
+
+    c = conn.cursor()
+    c.execute(sql, (user, location,))
+    conn.commit()
+
+    c.execute("SELECT user, location FROM noter WHERE id=?", (c.lastrowid,))
+    last = c.fetchone()
+
+    return last
+
+def clear_user(conn, user):
+
+    c = conn.cursor()
+    c.execute('SELECT id FROM noter WHERE user=?', (user,))
+
+    all = c.fetchmany(2)
+    if len(all) != 1:
+        c.close()
+        sys.exit('Looks like there are more than 1 {} on record! Contact mmartin@focusvision.com'.format(user))
+
+    c.execute('DELETE FROM noter WHERE id=?', c.fetchone())
+    print 'Noter file not found. Reconfiguring {}...'.format(user)
+
+
+def get_user(conn, user):
+
+    try:
+        c = conn.cursor()
+        c.execute("SELECT user, location FROM noter WHERE user=? ", [user])
+
+        userinfo = c.fetchone()
+        noter_exists = file_exists(userinfo[1])
+        if userinfo and noter_exists:
+            return userinfo
+
+        if not noter_exists:
+            clear_user(conn, user)
+
+        userinfo = create_user(conn, user)
+        return userinfo
+
+    except Exception, e:
+        print e
+
+    return None
+
+
+def get_or_create(db_file, user):
+
+    sql = """CREATE TABLE IF NOT EXISTS noter (
+              id INTEGER PRIMARY KEY,
+              user TEXT,
+              location TEXT
+          );"""
+    config = None
+    conn = connect(db_file)
+    if conn is not None:
+        create_table(conn, sql)
+
+        user, location = get_user(conn, user)
+        return user, location
+
+    conn.close()
--- noter.py
+++ noter.py
@@ -1,19 +1,39 @@
-import os, traceback
-from sys import exit, argv
+#!/usr/bin/env python2.7
+
+import os, traceback, getpass
+import sys
+import argparse
 from difflib import SequenceMatcher
+import yaml
+from get_config import get_or_create
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../accessdb')))
+# from accessdb import accessDB
+
 
 class Noter:
 
-    def __init__(self, configfile, search):
-        self.search = search
-        self.file = configfile
-        self.file1 = self.open_file()
+    def __init__(self, opt):
+        self.opt = opt
+        self.username = getpass.getuser()
+
+        # Symlink causes issues with dirname, get realpath first.
+        self.script_location = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
+        self.Database = '%s/noter.db' % self.script_location
+
+        self.noter_location = self.get_personal_file()[1]
+        self.noter_file = self.open_file(self.noter_location)
+
+        self.search = opt.search
         self.note = ''
         self.partial = ''
         self.notes = []
 
-    def open_file(self):
-        return open(self.file, 'r')
+
+    def get_personal_file(self):
+        return get_or_create(self.Database, self.username)
+
+    def open_file(self, notefile):
+        return open(notefile, 'r')
 
     def create_pair(self):
         self.notes.append((self.note, self.partial.rstrip('\n')))
@@ -40,34 +60,73 @@ class Noter:
             self.add_partial(line)
 
     def loop_file(self):
-        for line in self.file1.readlines():
+        for line in self.noter_file.readlines():
             self.determine(line)
 
         # Create last pair
         self.create_pair()
 
     def similar(self, a, b):
+        # list( set( [ 1, 2 ] ).symmetric_difference( set( [ 2, 3 ] ) ) )
         return SequenceMatcher(None, a.lower(), b.lower()).ratio()
 
-    def return_notes(self):
-        for note in self.notes:
-            note, notetext = note
-            similiar = self.similar(note, self.search)
 
-            if similiar > 0.75:
-                print '``{}\n{}'.format(note, notetext)
-                print '-'*15, '\n'
+    def print_top(self, top):
+
+        if top['top'] < self.opt.threshold:
+            print 'The closest I could find is:\n'
+
+        for item in top['matches']:
+            print item['text']
+            print '-'*15, '\n'
+
+
+    def return_notes(self):
+        top = {'top': 0, 'matches': []}
+        for note in self.notes:
+            notename, notetext = note
+
+            similiar = self.similar(notename, self.search)
+            text = '``{}\n{}'.format(notename, notetext)
+
+            if similiar > top['top']:
+                top['top'] = similiar
+                top['matches'] = [{'value': similiar, 'text': text}]
+
+            elif similiar > self.opt.threshold:
+                if top >= self.opt.threshold:
+                    top['matches'].append({'value': similiar, 'text': text})
+                else:
+                    top['matches'] = [{'value': similiar, 'text': text}]
+
+        self.print_top(top)
 
     def main(self):
         self.loop_file()
         self.return_notes()
 
 
+def get_options():
+    opt = argparse.ArgumentParser(add_help=None, usage='Help')
+    opt.add_argument("-h", "--help", action="store_true")
+    opt.add_argument("-f", "--file", type=str)
+    opt.add_argument("-t", "--threshold", type=float, default=0.75)
+    opt.add_argument("search", type=str)
+    return opt
+
+def print_help(opt):
+    print sys.exit(opt.print_help())
+
+
 if __name__ == '__main__':
     try:
-        noter = Noter(argv[1], ' '.join(argv[2:]))
+        o = get_options()
+        options = o.parse_args()
+        if options.help:
+            print_help(o)
+        noter = Noter(options)
         noter.main()
-        exit(0)
+        sys.exit(0)
     except KeyboardInterrupt, e:
         raise e
     except SystemExit, e:
